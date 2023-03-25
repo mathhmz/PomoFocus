36ce1e18bcf61ba1e854f63bef68ad65f70fdb17
mathhmz <rm551843@fiap.com.br>
2023-03-25 19:33:22 -0300

    Alteracoes nas modularizacoes do objeto

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 import {menu} from "./options.js"
 
-menu.play.addEventListener("click", () => menu.onClickTogglePlayAndPause());
-menu.pause.addEventListener("click", () => menu.onClickTogglePlayAndPause());
 
-menu.set.addEventListener("click", () => menu.onClickToggleSetAndStop())
-menu.stop.addEventListener("click", () => menu.onClickToggleSetAndStop())
-
-menu.musicOff.addEventListener("click", () => menu.onClickToggleMusic())
-menu.musicOn.addEventListener("click", () => menu.onClickToggleMusic())
+// Inicializa a funcao menu
+menu.init()
--- js/options.js
+++ js/options.js
@@ -1,25 +1,47 @@
+// Define uma função para alternar a visibilidade de dois elementos
+const toggleVisibility = (element1, element2) => {
+    element1.classList.toggle("hide");
+    element2.classList.toggle("hide");
+  };
+
+
+// Define um objeto que representa o menu e suas propriedades
 export const menu = {
     play: document.querySelector(".heroPlay"),
+
     pause: document.querySelector(".heroPause"),
+
     stop: document.querySelector(".heroStop"),
+
     set: document.querySelector(".heroSet"),
+
     musicOn: document.querySelector(".soundOn"),
+
     musicOff: document.querySelector(".soundOff"),
 
-
-    onClickTogglePlayAndPause(){
-        menu.play.classList.toggle("hide")
-        menu.pause.classList.toggle("hide")
-    },
-    onClickToggleSetAndStop(){
-        menu.set.classList.toggle("hide")
-        menu.stop.classList.toggle("hide")
-    },
-    onClickToggleMusic(){
-        menu.musicOff.classList.toggle("hide")
-        menu.musicOn.classList.toggle("hide")
-    }
-
     
+// Define métodos para alternar a visibilidade dos elementos
+     togglePlayPause(){
+        toggleVisibility(menu.play, menu.pause);
+      },
+      
+     toggleSetStop(){
+        toggleVisibility(menu.set, menu.stop);
+      },
+      
+     toggleMusic(){
+        toggleVisibility(menu.musicOff, menu.musicOn); 
+      },
+
+
+      // Adiciona os eventos de clique aos elementos do menu
+      init() {
+        this.play.addEventListener("click", this.togglePlayPause.bind(this));
+        this.pause.addEventListener("click", this.togglePlayPause.bind(this));
+        this.set.addEventListener("click", this.toggleSetStop.bind(this));
+        this.stop.addEventListener("click", this.toggleSetStop.bind(this));
+        this.musicOn.addEventListener("click", this.toggleMusic.bind(this));
+        this.musicOff.addEventListener("click", this.toggleMusic.bind(this));
+      },    
 
 }
