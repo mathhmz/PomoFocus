c686f0a61b1efb73131ff7d0fd2ff76bea943adb
mathhmz <rm551843@fiap.com.br>
2023-03-25 20:45:04 -0300

    Ajustes na interacao da interface

--- js/options.js
+++ js/options.js
@@ -8,40 +8,36 @@ const toggleVisibility = (element1, element2) => {
 // Define um objeto que representa o menu e suas propriedades
 export const menu = {
     play: document.querySelector(".heroPlay"),
-
     pause: document.querySelector(".heroPause"),
-
-    stop: document.querySelector(".heroStop"),
-
-    set: document.querySelector(".heroSet"),
-
+    stop: document.querySelector(".heroSet"),
+    set: document.querySelector(".heroStop"),
     musicOn: document.querySelector(".soundOn"),
-
     musicOff: document.querySelector(".soundOff"),
 
     
 // Define métodos para alternar a visibilidade dos elementos
-     togglePlayPause(){
-        toggleVisibility(menu.play, menu.pause);
-      },
-      
-     toggleSetStop(){
-        toggleVisibility(menu.set, menu.stop);
-      },
-      
-     toggleMusic(){
-        toggleVisibility(menu.musicOff, menu.musicOn); 
-      },
+        togglePlayPause() {
+            toggleVisibility(this.play, this.pause);
+            toggleVisibility(this.set, this.stop);
+        },
+        toggleSetStop() {
+            toggleVisibility(this.set, this.stop);
+            toggleVisibility(this.play, this.pause);
+
+        },
+        toggleMusic() {
+            toggleVisibility(this.musicOff, this.musicOn);
+        },
 
 
-      // Adiciona os eventos de clique aos elementos do menu
-      init() {
-        this.play.addEventListener("click", this.togglePlayPause.bind(this));
-        this.pause.addEventListener("click", this.togglePlayPause.bind(this));
-        this.set.addEventListener("click", this.toggleSetStop.bind(this));
-        this.stop.addEventListener("click", this.toggleSetStop.bind(this));
-        this.musicOn.addEventListener("click", this.toggleMusic.bind(this));
-        this.musicOff.addEventListener("click", this.toggleMusic.bind(this));
-      },    
+        // Adiciona os eventos de clique aos elementos do menu
+        init() {
+            this.play.addEventListener("click", this.togglePlayPause.bind(this));
+            this.pause.addEventListener("click", this.togglePlayPause.bind(this));
+            this.stop.addEventListener("click", this.toggleSetStop.bind(this));
+            this.musicOn.addEventListener("click", this.toggleMusic.bind(this));
+            this.musicOff.addEventListener("click", this.toggleMusic.bind(this));
+        },
+};
 
-}
+
